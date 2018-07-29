f6c89bc2df96e88f5f4bb1b036440b1406e37a87
rsangeetha28 <rsangeetha28@gmail.com>
2018-07-29 19:45:54 -0400

    Committing Services Code

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 
   var app = angular.module("githubViewer", []);
 
-  var MainController = function($scope, $http) {
+  /*var MainController = function(
+    $scope, $http, $interval, 
+    $log, $anchorScroll, $location) {*/
+    //or
+    var MainController = function(
+    $scope, github, $interval, 
+    $log, $anchorScroll, $location) {
 
-    var onUserComplete = function(response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url)
-           .then(onRepos, onError)
+    var onUserComplete = function(data) {
+      $scope.user = data;
+      //$http.get($scope.user.repos_url)
+      github.getRepos($scope.user)
+        .then(onRepos, onError)
     };
-    
-    var onRepos = function(response){
-      $scope.repos = response.data;
+
+    var onRepos = function(data) {
+      $scope.repos = data;
+      $location.hash("userDetails");
+      $anchorScroll();
     };
 
     var onError = function(reason) {
@@ -19,18 +28,42 @@
     };
 
     $scope.search = function(username) {
-      $http.get("https://api.github.com/users/" + username)
+      $log.info("Searching for "+username);
+     // $http.get("https://api.github.com/users/" + username)
+     github.getUser(username)
         .then(onUserComplete, onError);
+        if(countdownInterval){
+          $interval.cancel(countdownInterval);
+          $scope.countdown=null;
+        }
     };
 
+    var decrementCountdown = function() {
+      $scope.countdown = $scope.countdown - 1;
+      if ($scope.countdown < 1) {
+        $scope.search($scope.username);
+      }
+    };
+
+    var countdownInterval = null;
+    var startCountdown = function() {
+      //1st argument: A function that should be called repeatedly. 
+      //If no additional arguments are passed (see below), the function is called with the current iteration count.
+      //2nd argument:Number of milliseconds between each function call.
+      //3rd argument:Number of times to repeat. If not set, or 0, will repeat indefinitely.
+      countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+    };
 
     $scope.username = "angular";
     $scope.message = "Github Viewer";
     $scope.repoSortOrder = "-stargazers_count";
-
+    $scope.countdown = 5;
+    startCountdown();
 
   };
 
-  app.controller("MainController", ["$scope", "$http", MainController]);
+  app.controller("MainController", MainController);
+  //or
+  //app.controller("MainController", ["$scope", "$http", "$interval", "$log", MainController]);
 
 }());
